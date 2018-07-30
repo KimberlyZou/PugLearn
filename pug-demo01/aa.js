1eb771dfcd2d912da77deb6bde62909c42ab8110
KimberlyZou1 <kimberly.zou@perficient.com>
2018-07-30 17:59:39 +0800

    ss

---
+++ pug-demo01/aa.js
@@ -0,0 +1,24 @@
+// var aa = {
+//     color: "red",
+//     getColor: function () {
+//         return this.color;
+//     }
+// };
+// var getColor = aa.getColor;
+// var color = "blue";
+// console.log(aa.getColor());
+// console.log(getColor());
+// console.log(this);
+
+// const a = {
+//     i: 1,
+//     toString: function () {
+//         return a.i++;
+//     }
+// }
+
+// if (a == 1 && a == 2 && a == 3) {
+//     console.log('Hello World!');
+
+// }
+// /^(0|([1-9][0-9]*))$/
